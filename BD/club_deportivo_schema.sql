6efd3661ddc1b7a9b34c14292c02f25ad55d777e
Enzo Giangreco <109107113+enluuca@users.noreply.github.com>
2025-05-13 20:13:17 -0300

    Add files via upload

---
+++ BD/club_deportivo_schema.sql
@@ -0,0 +1,221 @@
+-- MySQL dump 10.13  Distrib 8.0.42, for Win64 (x86_64)
+--
+-- Host: localhost    Database: clubdeportivo
+-- ------------------------------------------------------
+-- Server version	8.0.42
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `actividad`
+--
+
+DROP TABLE IF EXISTS `actividad`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `actividad` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(50) DEFAULT NULL,
+  `costo` decimal(10,2) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `actividad`
+--
+
+LOCK TABLES `actividad` WRITE;
+/*!40000 ALTER TABLE `actividad` DISABLE KEYS */;
+/*!40000 ALTER TABLE `actividad` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `cliente`
+--
+
+DROP TABLE IF EXISTS `cliente`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `cliente` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(50) DEFAULT NULL,
+  `apellido` varchar(50) DEFAULT NULL,
+  `dni` int DEFAULT NULL,
+  `fechaNacimiento` date DEFAULT NULL,
+  `direccion` varchar(100) DEFAULT NULL,
+  `telefono` varchar(20) DEFAULT NULL,
+  `aptoFisico` tinyint(1) DEFAULT NULL,
+  `fechaAlta` date DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `dni` (`dni`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cliente`
+--
+
+LOCK TABLES `cliente` WRITE;
+/*!40000 ALTER TABLE `cliente` DISABLE KEYS */;
+/*!40000 ALTER TABLE `cliente` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `cuota`
+--
+
+DROP TABLE IF EXISTS `cuota`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `cuota` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `idSocio` int DEFAULT NULL,
+  `fechaPago` date DEFAULT NULL,
+  `monto` decimal(10,2) DEFAULT NULL,
+  `medioPago` varchar(30) DEFAULT NULL,
+  `cantidadCuotas` int DEFAULT NULL,
+  `fechaVencimiento` date DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `idSocio` (`idSocio`),
+  CONSTRAINT `cuota_ibfk_1` FOREIGN KEY (`idSocio`) REFERENCES `socio` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cuota`
+--
+
+LOCK TABLES `cuota` WRITE;
+/*!40000 ALTER TABLE `cuota` DISABLE KEYS */;
+/*!40000 ALTER TABLE `cuota` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `nosocio`
+--
+
+DROP TABLE IF EXISTS `nosocio`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `nosocio` (
+  `id` int NOT NULL,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `nosocio_ibfk_1` FOREIGN KEY (`id`) REFERENCES `cliente` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `nosocio`
+--
+
+LOCK TABLES `nosocio` WRITE;
+/*!40000 ALTER TABLE `nosocio` DISABLE KEYS */;
+/*!40000 ALTER TABLE `nosocio` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `registroactividad`
+--
+
+DROP TABLE IF EXISTS `registroactividad`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `registroactividad` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `idNoSocio` int DEFAULT NULL,
+  `idActividad` int DEFAULT NULL,
+  `fechaPago` date DEFAULT NULL,
+  `montoCobrado` decimal(10,2) DEFAULT NULL,
+  `medioPago` varchar(30) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `idNoSocio` (`idNoSocio`),
+  KEY `idActividad` (`idActividad`),
+  CONSTRAINT `registroactividad_ibfk_1` FOREIGN KEY (`idNoSocio`) REFERENCES `nosocio` (`id`),
+  CONSTRAINT `registroactividad_ibfk_2` FOREIGN KEY (`idActividad`) REFERENCES `actividad` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `registroactividad`
+--
+
+LOCK TABLES `registroactividad` WRITE;
+/*!40000 ALTER TABLE `registroactividad` DISABLE KEYS */;
+/*!40000 ALTER TABLE `registroactividad` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `socio`
+--
+
+DROP TABLE IF EXISTS `socio`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `socio` (
+  `id` int NOT NULL,
+  `fechaInscripcion` date DEFAULT NULL,
+  `fechaVencimientoCuota` date DEFAULT NULL,
+  `numeroCarnet` int DEFAULT NULL,
+  `carnetEntregado` tinyint(1) DEFAULT NULL,
+  `estado` varchar(20) DEFAULT NULL,
+  `fechaBaja` date DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `socio_ibfk_1` FOREIGN KEY (`id`) REFERENCES `cliente` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `socio`
+--
+
+LOCK TABLES `socio` WRITE;
+/*!40000 ALTER TABLE `socio` DISABLE KEYS */;
+/*!40000 ALTER TABLE `socio` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `usuarios`
+--
+
+DROP TABLE IF EXISTS `usuarios`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `usuarios` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `usuario` varchar(50) DEFAULT NULL,
+  `clave` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `usuario` (`usuario`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usuarios`
+--
+
+LOCK TABLES `usuarios` WRITE;
+/*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
+INSERT INTO `usuarios` VALUES (1,'admin','1234');
+/*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-05-11 20:43:20
